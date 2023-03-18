163f524d5a9e86b8bc6208ca57c4cc8c1a4c70a1
EC2 Default User <ec2-user@ip-172-31-2-229.ap-northeast-1.compute.internal>
2023-03-18 15:31:07 +0000

    [Add]投稿に評価機能

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -3,12 +3,18 @@ class Public::PostsController < ApplicationController
     @post = Post.new
     @user = current_user
     @towns = Town.all
+    @categories = Category.all
   end
   
   def create
+    post = Post.new(post_params)
+    post.user_id = current_user.id
+    post.save
+    redirect_to posts_path
   end
 
   def index
+    @posts = Post.all
   end
 
   def edit
@@ -16,5 +22,10 @@ class Public::PostsController < ApplicationController
   
   def update
   end
-  
+
+private
+  def post_params
+    params.require(:post).permit(:image,:town_id,:category_id,:title,:star,:body)
+  end
+
 end
