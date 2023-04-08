902479bae6be1ca159fd0d69ff9e88c6a24194d7
EC2 Default User <ec2-user@ip-172-31-2-229.ap-northeast-1.compute.internal>
2023-04-08 16:55:45 +0000

    [Add]地図上で訪問先市町村にピン

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -9,5 +9,15 @@ class Public::HomesController < ApplicationController
     # 公開ユーザーの投稿を全件取得
     @posts = Post.where(user_id: released_user_id)
     @posts = Post.where(user_id: released_user_id).order(created_at: :desc)
+    
+    # 地図上で投稿した市町村にポイントを打つ
+    if user_signed_in?
+      user_posts = current_user.posts
+      user_post_town_id = []
+      user_posts.each do |user_post|
+        user_post_town_id << user_post.town_id
+      end
+      @towns = Town.where(id: user_post_town_id)
+    end
   end
 end
