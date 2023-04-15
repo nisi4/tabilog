450c872b80f80aa854817b5f50860bd33da4dd85
EC2 Default User <ec2-user@ip-172-31-2-229.ap-northeast-1.compute.internal>
2023-04-15 11:13:02 +0000

    [Add]無限スクロール

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -37,9 +37,9 @@ class Public::PostsController < ApplicationController
     
     # ここは params[:sort]が存在しているかどうかチェックするのが正しい
     if params[:sort] == "likes" || params[:sort] == "dislikes"
-      @posts = @posts.post_sort(params[:sort])
+      @posts = @posts.post_sort(params[:sort]).page(params[:page]).per(8)
     else
-      @posts = @posts.order(created_at: :desc)
+      @posts = @posts.order(created_at: :desc).page(params[:page]).per(8)
     end
     
     @keyword = params[:keyword]
